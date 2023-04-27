b9ba3ae37749de29ef7081de1516ebfe74f78034
Nick Marsalek <nickmarsalek@yahoo.com>
2023-04-26 23:31:56 -0400

    Added Best Scores at end

--- game_over.py
+++ game_over.py
@@ -8,8 +8,27 @@ pygame.display.set_caption("Game Over")
 FONT_SIZE = 48
 SCORE_FONT = pygame.font.Font("Assets/8bitfont.ttf", FONT_SIZE)
 
+def get_best_scores():
+    # Open the file and read the scores
+    with open('scores.txt', 'r') as f:
+        scores = [int(line.strip()) for line in f.readlines()]
+
+    # Sort the scores in descending order
+    scores.sort(reverse=True)
+
+    return scores[:3]
+
+def add_score_to_file(score):
+    # Open the file and append the score
+    with open('scores.txt', 'a') as f:
+        f.write(str(score) + '\n')
+
 def game_over(display_surface):
     score = 40
+
+    # Add the current score to the file
+    add_score_to_file(score)
+
     while True:
         events = pygame.event.get()
         for event in events:
@@ -28,6 +47,16 @@ def game_over(display_surface):
         score_text = SCORE_FONT.render(f"Score: {score}", True, (255, 255, 255))
         score_rect = score_text.get_rect(center=(800 // 2, 600 // 2 - FONT_SIZE))
         display_surface.blit(score_text, score_rect)
+
+        # Display the best scores
+        best_scores = get_best_scores()
+        best_scores_text = SCORE_FONT.render("Best Scores:", True, (255, 255, 255))
+        display_surface.blit(best_scores_text, (50, 50))
+        y = 100
+        for i, score in enumerate(best_scores):
+            score_text = SCORE_FONT.render(f"{i+1}. {score}", True, (255, 255, 255))
+            display_surface.blit(score_text, (50, y))
+            y += FONT_SIZE
 
         # Draw the exit button
         exit_button_text = SCORE_FONT.render("Exit", True, (255, 255, 255))
